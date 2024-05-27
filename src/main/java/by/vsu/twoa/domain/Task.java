3884a43faedcee83219b3e8bfe9a6715e5edb0b6
Sergey Yermochenko <yermochenko@gmail.com>
2024-05-27 13:58:20 +0300

    FR3 и FR4 завершены

--- src/main/java/by/vsu/twoa/domain/Task.java
+++ src/main/java/by/vsu/twoa/domain/Task.java
@@ -1,11 +1,18 @@
 package by.vsu.twoa.domain;
 
+import by.vsu.twoa.geometry.Point;
+import by.vsu.twoa.geometry.Triangle;
+import by.vsu.twoa.geometry.relpos.PointTriangleRelativePosition;
+
 import java.util.Date;
 
 public class Task extends Entity {
 	private User owner;
 	private String name;
 	private Date created;
+	private Triangle triangle;
+	private Point point;
+	private PointTriangleRelativePosition pointTriangleRelativePosition;
 
 	public User getOwner() {
 		return owner;
@@ -29,5 +36,29 @@ public class Task extends Entity {
 
 	public void setCreated(Date created) {
 		this.created = created;
+	}
+
+	public Triangle getTriangle() {
+		return triangle;
+	}
+
+	public void setTriangle(Triangle triangle) {
+		this.triangle = triangle;
+	}
+
+	public Point getPoint() {
+		return point;
+	}
+
+	public void setPoint(Point point) {
+		this.point = point;
+	}
+
+	public PointTriangleRelativePosition getPointTriangleRelativePosition() {
+		return pointTriangleRelativePosition;
+	}
+
+	public void setPointTriangleRelativePosition(PointTriangleRelativePosition pointTriangleRelativePosition) {
+		this.pointTriangleRelativePosition = pointTriangleRelativePosition;
 	}
 }
--- src/main/java/by/vsu/twoa/service/TaskService.java
+++ src/main/java/by/vsu/twoa/service/TaskService.java
@@ -5,10 +5,16 @@ import by.vsu.twoa.dao.TaskDao;
 import by.vsu.twoa.dao.UserDao;
 import by.vsu.twoa.domain.Task;
 import by.vsu.twoa.domain.User;
+import by.vsu.twoa.geometry.*;
+import by.vsu.twoa.geometry.relpos.PointTriangleRelativePosition;
 import by.vsu.twoa.service.exception.ServiceException;
+import by.vsu.twoa.service.exception.TaskNotExistsException;
 import by.vsu.twoa.service.exception.UserNotExistsException;
 
+import java.util.Date;
 import java.util.List;
+
+import static java.lang.Math.abs;
 
 public class TaskService {
 	private TaskDao taskDao;
@@ -31,5 +37,64 @@ public class TaskService {
 		} catch(DaoException e) {
 			throw new ServiceException(e);
 		}
+	}
+
+
+	public Task findById(Integer id) throws ServiceException {
+		try {
+			Task task = taskDao.read(id).orElseThrow(() -> new TaskNotExistsException(id));
+			task.setOwner(userDao.read(task.getOwner().getId()).orElseThrow(() -> new UserNotExistsException(id)));
+			Triangle triangle = task.getTriangle();
+			Point a = triangle.getVertex1();
+			Point b = triangle.getVertex2();
+			Point c = triangle.getVertex3();
+			Point p = task.getPoint();
+			if(equal(p, a) || equal(p, b) || equal(p, c)) {
+				task.setPointTriangleRelativePosition(PointTriangleRelativePosition.VERTEX);
+			} else {
+				Line ab = new Line(a, b);
+				Line bc = new Line(b, c);
+				Line ac = new Line(a, c);
+				double pAB = lineEq(ab, p);
+				double pBC = lineEq(bc, p);
+				double pAC = lineEq(ac, p);
+				if(abs(pAB) < 0.0001 && abs(pBC) < 0.0001 && abs(pAC) < 0.0001) {
+					task.setPointTriangleRelativePosition(PointTriangleRelativePosition.ONSIDE);
+				} else {
+					double cAB = lineEq(ab, c);
+					double aBC = lineEq(bc, a);
+					double bAC = lineEq(ac, b);
+					if(pAB * cAB < 0 && pBC * aBC < 0 && pAC * bAC < 0) {
+						task.setPointTriangleRelativePosition(PointTriangleRelativePosition.INSIDE);
+					} else {
+						task.setPointTriangleRelativePosition(PointTriangleRelativePosition.OUTSIDE);
+					}
+				}
+			}
+			return task;
+		} catch(DaoException e) {
+			throw new ServiceException(e);
+		}
+	}
+
+	public Integer save(Task task) throws ServiceException {
+		try {
+			if(task.getId() == null) {
+				task.setCreated(new Date(0));
+				return taskDao.create(task);
+			} else {
+				throw new RuntimeException("Update operation not implemented yet");
+			}
+		} catch(DaoException e) {
+			throw new ServiceException(e);
+		}
+	}
+
+	private static boolean equal(Point a, Point b) {
+		return abs(a.getX() - b.getX()) < 0.0001 && abs(a.getY() - b.getY()) < 0.0001;
+	}
+
+	private static double lineEq(Line line, Point point) {
+		return line.getA() * point.getX() + line.getB() * point.getY() + line.getC();
 	}
 }
